8a803666c12b1de85f39ba2c8f4102fbdb76c672
Kavish Toraskar <kavishtoraskar24@gmail.com>
2025-06-18 23:49:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const commentRoutes = require('./routes/comments');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: FRONTEND_UR, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
